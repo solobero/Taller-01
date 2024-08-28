634c86495eae50c1594b7e49be882d0b5c4d9696
Vero <vzapatav@eafit.edu.co>
2024-08-28 08:19:32 -0500

    Cambios codigo limpio

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,39 +6,40 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
-
     public function index(): View
     {
         $viewData = [];
-        $viewData["title"] = "Products - Online Store";
-        $viewData["subtitle"] =  "List of products";
-        $viewData["products"] = Product::all();
+        $viewData['title'] = "Products - Online Store";
+        $viewData['subtitle'] = "List of products";
+        $viewData['products'] = Product::all();
         return view('product.index')->with("viewData", $viewData);
     }
 
-    public function show(string $id) : View
+    public function show(string $id): View
     {
         $viewData = [];
         $product = Product::findOrFail($id);
-        $viewData["title"] = $product["name"]." IMEC - Online Store";
-        $viewData["subtitle"] =  $product["name"]." - Product information";
-        $viewData["product"] = $product;
+        $viewData['title'] = $product->getName() . " IMEC - Online Store";
+        $viewData['subtitle'] = $product->getName() . " - Product information";
+        $viewData['product'] = $product;
         return view('product.show')->with("viewData", $viewData);
     }
 
     public function create(): View
     {
         $viewData = []; //to be sent to the view
-        $viewData["title"] = "Create product";
-        return view('product.create')->with("viewData",$viewData);
+        $viewData['title'] = "Create product";
+        return view('product.create')->with("viewData", $viewData);
     }
 
-    public function save(Request $request): \Illuminate\Http\RedirectResponse
+    public function save(Request $request): RedirectResponse
     {
-
+        //mover la validación al modelo como funcion ó usar laravel form request y luego usar aqui
+        //Product::validate($request)
         $request->validate([
             'name' => 'required|string|max:255',
             'description' => "string",
@@ -53,24 +54,23 @@ class ProductController extends Controller
             $imagePath = $image->store('images', 'public');
         }
 
-        Product::create([
+        Product::create($request->only([
             'name' => $request->input('name'),
             'description' => $request->input('description'),
             'image' => $imagePath,
             'price' => $request->input('price'),
             'warranty' => $request->input('warranty'),
-        ]);
+        ]));
 
         return redirect()->route('product.create')->with('status', 'Item created successfully!');
     }
 
-    public function delete($id): \Illuminate\Http\RedirectResponse
+    public function delete($id): RedirectResponse
     {
-
         $product = Product::findOrFail($id);
 
-        if ($product->image && Storage::exists('public/' . $product->image)) {
-            Storage::delete('public/' . $product->image);
+        if ($product->getImage() && Storage::exists('public/' . $product->getImage())) {
+            Storage::delete('public/' . $product->getImage());
         }
 
         $product->delete();
